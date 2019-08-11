9735c72406c312b8d8ccc40b0a6561b7582aa46a
Justin Aquadro <jaquadro@gmail.com>
2019-08-11 01:56:45 -0400

    Ready to release 2.0.0.0

--- src/main/java/com/jaquadro/minecraft/hungerstrike/HungerStrike.java
+++ src/main/java/com/jaquadro/minecraft/hungerstrike/HungerStrike.java
@@ -5,15 +5,18 @@ import com.jaquadro.minecraft.hungerstrike.network.PacketHandler;
 import com.jaquadro.minecraft.hungerstrike.proxy.ClientProxy;
 import com.jaquadro.minecraft.hungerstrike.proxy.CommonProxy;
 import com.jaquadro.minecraft.hungerstrike.proxy.ServerProxy;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.IForgeRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -45,13 +48,13 @@ public class HungerStrike
         PacketHandler.init();
         ExtendedPlayerHandler.register();
 
-        /*IForgeRegistry<Item> itemRegistry = GameRegistry.findRegistry(Item.class)
-        if (config.getFoodStackSize() > -1) {
+        IForgeRegistry<Item> itemRegistry = GameRegistry.findRegistry(Item.class);
+        if (ModConfig.GENERAL.foodStackSize.get() > -1) {
             for (Item item : itemRegistry) {
                 if (item != null && item.isFood())
-                    item.setMaxStackSize(config.getFoodStackSize());
+                    item.maxStackSize = ModConfig.GENERAL.foodStackSize.get();
             }
-        }*/
+        }
     }
 
     @SubscribeEvent
--- src/main/java/com/jaquadro/minecraft/hungerstrike/ModConfig.java
+++ src/main/java/com/jaquadro/minecraft/hungerstrike/ModConfig.java
@@ -27,33 +27,25 @@ public class ModConfig
                 .defineEnum("mode", Mode.ALL);
 
             foodHealFactor = builder
-                .comment("How to translate food points into heart points when",
-                    "consuming food. At the default value of 0.5, food fills",
-                    "your heart bar at half the rate it would fill hunger.")
+                .comment("How to translate food points into heart points when consuming food.",
+                    "At the default value of 0.5, food fills your heart bar at half the rate it would fill hunger.")
                 .define("foodHealFactor", 0.5);
 
             foodStackSize = builder
                 .comment("Globally overrides the maximum stack size of food items.",
-                    "This property affects all Vanilla and Mod food items that",
-                    "derive from ItemFood.",
-                    "Set to -1 to retain the default stack size of each food",
-                    "item.  Note: This will affect the entire server, not just",
-                    "players on hunger strike.")
+                    "This property affects all Vanilla and Mod food items that derive from ItemFood.",
+                    "Set to -1 to retain the default stack size of each food item.  Note: This will affect the entire server, not just players on hunger strike.",
+                    "WARNING: Setting this property may result in unexpected behavior with other mods.")
                 .defineInRange("maxFoodStackSize", -1, -1, 64);
 
             hideHungerBar = builder
-                .comment("Controls whether or not the hunger bar is hidden for",
-                    "players on hunger strike.  If the hunger bar is left",
-                    "visible, it will remain filled at half capacity, except",
-                    "when certain potion effects are active like hunger and",
-                    "regeneration.")
+                .comment("Controls whether or not the hunger bar is hidden for players on hunger strike.",
+                    "If the hunger bar is left visible, it will remain filled at half capacity, except when certain potion effects are active like hunger and regeneration.")
                 .define("hideHungerBar", true);
 
             hungerBaseline = builder
-                .comment("The default hunger level when no status effects are",
-                    "active.  Valid range is [1 - 20], with 20 being fully",
-                    "filled, and 10 being half-filled.  The default value is",
-                    "10, which disables health regen but allows sprinting.")
+                .comment("The default hunger level when no status effects are active.",
+                    "Valid range is [1 - 20], with 20 being fully filled, and 10 being half-filled.  The default value is 10, which disables health regen but allows sprinting.")
                 .defineInRange("hungerBaseline", 10, 1, 20);
 
             builder.pop();
